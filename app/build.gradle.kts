8b213ca44460160aeb9256a611f429f42914ce4a
Thomas Stromberg <t+github@codeGROOVE.dev>
2025-10-11 16:33:04 +0200

    v0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.heisenberg.lux"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
     }
 
     signingConfigs {
